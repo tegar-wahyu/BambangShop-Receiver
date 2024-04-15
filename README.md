2a7e855caaacc0c9d9cdd0e41eedb1eb4cc14c8f
tegar-wahyu <tegar.wahyu@ui.ac.id>
2024-04-15 16:26:14 +0700

    Implement unsubscribe function in Notification controller.

--- README.md
+++ README.md
@@ -72,7 +72,7 @@ You can install Postman via this website: https://www.postman.com/downloads/
     -   [x] Commit: `Implement subscribe function in Notification service.`
     -   [x] Commit: `Implement subscribe function in Notification controller.`
     -   [x] Commit: `Implement unsubscribe function in Notification service.`
-    -   [ ] Commit: `Implement unsubscribe function in Notification controller.`
+    -   [x] Commit: `Implement unsubscribe function in Notification controller.`
     -   [ ] Commit: `Implement receive_notification function in Notification service.`
     -   [ ] Commit: `Implement receive function in Notification controller.`
     -   [ ] Commit: `Implement list_messages function in Notification service.`
--- src/controller/notification.rs
+++ src/controller/notification.rs
@@ -1,4 +1,5 @@
 use rocket::serde::json::Json;
+
 use bambangshop_receiver::Result;
 use crate::model::notification::Notification;
 use crate::model::subscriber::SubscriberRequest;
@@ -7,6 +8,14 @@ use crate::service::notification::NotificationService;
 #[get("/subscribe/<product_type>")]
 pub fn subscribe(product_type: &str)->Result<Json<SubscriberRequest>> {
     return match NotificationService::subscribe(product_type) {
+        Ok(f) => Ok(Json::from(f)),
+        Err(e) => Err(e)
+    }
+}
+
+#[get("/unsubscribe/<product_type>")]
+pub fn unsubscribe(product_type: &str) -> Result<Json<SubscriberRequest>> {
+    return match NotificationService::unsubscribe(product_type) {
         Ok(f) => Ok(Json::from(f)),
         Err(e) => Err(e)
     }
